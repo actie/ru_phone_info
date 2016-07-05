31d4da8b7d56b6880a854d5ab9aad93a5732c5a1
b0nn1e <andrey.b0nn1e@gmail.com>
2016-07-05 17:37:09 +0300

    Added regions code

--- spec/ru_phone_info_spec.rb
+++ spec/ru_phone_info_spec.rb
@@ -14,18 +14,21 @@ describe RuPhoneInfo do
     subject = RuPhoneInfo.get('9012653475')
     expect(subject[:region]).to eq 'Пермский край'
     expect(subject[:operator]).to eq 'ОАО Ростелеком'
+    expect(subject[:region_code]).to eq 'PER'
   end
 
   it 'find my phone' do
     subject = RuPhoneInfo.get('89891676438')
     expect(subject[:region]).to eq 'Краснодарский край'
     expect(subject[:operator]).to eq 'ОАО Мобильные ТелеСистемы'
+    expect(subject[:region_code]).to eq 'KDA'
   end
 
   it 'find other phone' do
     subject = RuPhoneInfo.get('+7 909 444 45 13')
     expect(subject[:region]).to eq 'Краснодарский край'
     expect(subject[:operator]).to eq 'ОАО Вымпел-Коммуникации'
+    expect(subject[:region_code]).to eq 'KDA'
   end
 
   it 'find phone with 8' do
@@ -38,5 +41,12 @@ describe RuPhoneInfo do
 
   it 'find phone with spaces and brackets' do
     expect(RuPhoneInfo.get('+7 (989) 167 64 38')).not_to be nil
+  end
+
+  it 'for any phone can be finded region code' do
+    RuPhoneInfo.mobile_phones.map do |row|
+      subject = RuPhoneInfo.get_region_code(row[5])
+      expect(subject).not_to be nil
+    end
   end
 end
